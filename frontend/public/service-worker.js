b54a93bf9d1d7c938009c25091485d7d89133839
kazadidieumerci70-hash <kazadidieumerci70-hash@users.noreply.github.com>
2026-09-17 15:40:50 +0200

    Use official logo as favicon

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "orvix-app-v1";
-const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest", "/orvix-icon.svg"];
+const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest", "/orvix-logo.jpeg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
